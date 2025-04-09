130aaa6ca864e661a84d453f6c9f100dd24d1084
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2025-04-09 10:34:23 +0200

    Test training model

---
+++ training/script.js
@@ -0,0 +1,28 @@
+import posedata from './data.json' with {type: "json"}
+let nn
+
+function startTraining() {
+    ml5.setBackend('webgl')
+    nn = ml5.neuralNetwork({task: 'classification', debug: true})
+    console.log(nn)
+
+    for(let pose of posedata) {
+        // console.log(pose)
+        nn.addData(pose.points, {label: pose.label})
+    }
+
+    nn.normalizeData()
+    nn.train({epochs:40}, finishedTraining)
+}
+
+function finishedTraining() {
+    console.log("finished training!")
+    let demopose = posedata[10].data
+    nn.classify(demopose, (results) => {
+        console.log(`i think this is a ${results[0].label}`)
+        console.log(`${(results[0].confidence.toFixed(2)) * 100}% sure`)
+    })
+    nn.save()
+}
+
+startTraining()
